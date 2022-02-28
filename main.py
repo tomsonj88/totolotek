03a048ce1cc7a84fb17da8a65d5dba70676f9f15
Tomson <tomsonj88@wp.pl>
2022-02-28 01:51:20 +0100

    Add function to generate six unique numbers

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 """
 ToDo dopisac doscrstringa
 """
-from random import randint
+import random
 
 win = False
 
 
-def generate_numbers(start, stop):
-    pass
+def generate_numbers(start=1, stop=49, numbers_quantity=6):
+    numbers = set(random.sample(range(start, stop), numbers_quantity))
+    return numbers
+
+generate_numbers()
